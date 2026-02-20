6ab0e3fde8ec9a29d5e61095907eb04192c50fdd
Olga Homenko <enima.est@gmail.com>
2026-02-20 16:00:35 +0200

    Add project files

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const modal = document.querySelector('.beckdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
@@ -6,3 +7,13 @@ const toggModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggModal);
 modalBtnClose.addEventListener('click', toggModal);
+=======
+const modal = document.querySelector('.backdrop');
+const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+
+const toggleModal = () => modal.classList.toggle('is-hidden');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
+>>>>>>> 7444e74 (Add project files)
